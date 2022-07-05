d5b57864cb49dc168e3f124882ecd84b8b7e590b
Roger Roldan <78450150+rroldan08@users.noreply.github.com>
2022-07-05 03:12:15 -0700

    Update fibonacciSequence.js

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -1,10 +1,16 @@
-
+// function that prints the first ten digits of the fibonacci sequence
 function fibSequence(){
+
+/* num2 temporarily holds a number of the fib sequence, num1 is used to store the sum of num2 
+and num1 and turn to num3, and num3 to become num2, or the next number of the sequence.
+*/
+
     let num1 = 0;
     let num2 = 1;
     let num3 = 0;
     
-    for(let i = 0; i < 5; i++){
+    // loops to print out the first ten numbers of the fib sequence.
+    for(let i = 0; i < 10; i++){
         console.log(num2);
         num3 = num2;
         num2 += num1;
@@ -12,4 +18,6 @@ function fibSequence(){
     }
 }
 
+
+// executes function
 console.log(fibSequence());
